f36b57f0d9e00dd9a52e810ab72722f34116eaae
Lorenzo <lorenzozar@gmail.com>
2022-10-21 07:49:12 +0300

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BooksComponent } from './components/books/books.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'books',
+    component: BooksComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TemplateFormComponent } from './components/template-form/template-form.component';
 import { ReactiveFormComponent } from './components/reactive-form/reactive-form.component';
@@ -11,8 +11,16 @@ import { ReadingComponent } from './components/books/reading/reading.component';
 import { HistoryComponent } from './components/books/history/history.component';
 
 @NgModule({
-  declarations: [AppComponent, TemplateFormComponent, ReactiveFormComponent, BooksComponent, WishListComponent, ReadingComponent, HistoryComponent],
-  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
+  declarations: [
+    AppComponent,
+    TemplateFormComponent,
+    ReactiveFormComponent,
+    BooksComponent,
+    WishListComponent,
+    ReadingComponent,
+    HistoryComponent,
+  ],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
