cc12a101586ee29083bd22ffe2357b0ae6470c0c
Lorenzo <lorenzozar@gmail.com>
2022-10-31 18:14:09 +0200

    Add basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BooksComponent } from './components/books/books.component';
+import { FormsComponent } from './components/forms/forms.component';
+import { HomeComponent } from './components/home/home.component';
+import { SubjectComponent } from './components/rxjs/subject/subject.component';
 
 const routes: Routes = [
   {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
     path: 'books',
     component: BooksComponent,
+  },
+  {
+    path: 'forms',
+    component: FormsComponent,
+  },
+  {
+    path: 'subject',
+    component: SubjectComponent,
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TemplateFormComponent } from './components/template-form/template-form.component';
-import { ReactiveFormComponent } from './components/reactive-form/reactive-form.component';
+import { TemplateFormComponent } from './components/forms/template-form/template-form.component';
+import { ReactiveFormComponent } from './components/forms/reactive-form/reactive-form.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BooksComponent } from './components/books/books.component';
 import { WishListComponent } from './components/books/wish-list/wish-list.component';
 import { ReadingComponent } from './components/books/reading/reading.component';
 import { HistoryComponent } from './components/books/history/history.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FormsComponent } from './components/forms/forms.component';
+import { HomeComponent } from './components/home/home.component';
+import { SubjectComponent } from './components/rxjs/subject/subject.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +23,10 @@ import { HistoryComponent } from './components/books/history/history.component';
     WishListComponent,
     ReadingComponent,
     HistoryComponent,
+    HeaderComponent,
+    FormsComponent,
+    HomeComponent,
+    SubjectComponent,
   ],
   imports: [BrowserModule, FormsModule, ReactiveFormsModule, AppRoutingModule],
   providers: [],
